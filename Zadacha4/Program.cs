a5161f05b56c624485b413bbb23cb000eccff564
Marina <marishka-2008-91@mail.ru>
2022-08-31 13:30:10 +0300

    z4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a <= n){
+    if (a % 2 == 0){
+        Console.WriteLine(a);
+    }
+    a++;
+}
+    

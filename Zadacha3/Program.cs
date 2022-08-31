2c1ea4e4747b02a6b7735c4b2141854f03e2fcd0
Marina <marishka-2008-91@mail.ru>
2022-08-31 13:19:17 +0300

    z4

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else if (a % 2 == 1 )
+{
+    Console.WriteLine("Число нечетное");
+}

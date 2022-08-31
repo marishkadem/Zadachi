8c6129cd06b71b30bf08d9e95ebd9d7baf1a47b9
Marina <marishka-2008-91@mail.ru>
2022-08-31 13:05:57 +0300

    z2.2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.Write("Введите 1 число: ");
+﻿System.Console.Write("Введите 1 число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2 число: ");
+System.Console.Write("Введите 2 число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 3 число: ");
+System.Console.Write("Введите 3 число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 

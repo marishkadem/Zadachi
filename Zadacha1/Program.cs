e8fa9e8b432496497435f518fb9cdbff3e91bce1
Marina <marishka-2008-91@mail.ru>
2022-08-31 11:32:48 +0300

    1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b){
+    System.Console.WriteLine("Максимальным числом является  "); Console.WriteLine(a);
+    System.Console.WriteLine("Минимальным числом является  ");
+      Console.WriteLine(b);
+}
+else{
+    System.Console.WriteLine("Максимальным числом является  "); Console.WriteLine(b);
+    System.Console.WriteLine("Минимальным числом является  ");
+      Console.WriteLine(a);
+}
